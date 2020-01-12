8fa96286038e1482a16e71a036b34cbc76d9c55b
JoaoGuimaraes22 <jssgmrs22@gmail.com>
2020-01-12 23:38:45 +0000

    Added Project

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../css/main.css";
+import project from "../img/project.png";
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 
 const Projects = () => {
   return (
@@ -13,28 +15,38 @@ const Projects = () => {
       </div>
       <div className="projects-container">
         <div className="project-1">
-          <div className="project-container"></div>
-          <div className="project-text">
-            <p className="featured">Featured Project</p>
-            <h3 className="project-name">Project Name</h3>
+          <div className="project-main-part">
+            <div className="project-text">
+              <div className="text-main-text">
+                <p className="featured">Featured Project</p>
+                <h3 className="project-name">Project Name</h3>
+              </div>
+              <div className="icons">
+                <FaGithub className="icon" />
+                <FaExternalLinkAlt className="icon" />
+              </div>
+            </div>
+            <div className="project-description">
+              <p className="project-desc-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+                saepe praesentium architecto consequuntur quis aliquid.
+                Temporibus quam sequi corporis veniam vitae voluptatem neque
+              </p>
+            </div>
+            <ul className="project-techs">
+              <li className="tech-name">C#</li>
+              <li className="tech-name">Javascript</li>
+              <li className="tech-name">SASS</li>
+            </ul>
           </div>
-          <div className="project-description">
-            <p className="project-desc-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
-              saepe praesentium architecto consequuntur quis aliquid. Temporibus
-              quam sequi corporis veniam vitae voluptatem neque nulla voluptates
-              illum.
-            </p>
+
+          <div className="project-img-part">
+            <img className="project-container" src={project}></img>
+            <div className="overlay">
+              <div className="ov-text">See project!</div>
+            </div>
           </div>
         </div>
-        <ul className="project-techs">
-          <li className="tech-name"></li>
-          <li className="tech-name"></li>
-          <li className="tech-name"></li>
-          <li className="tech-name"></li>
-        </ul>
-        <div className="project-2"></div>
-        <div className="project-3"></div>
       </div>
     </div>
   );
