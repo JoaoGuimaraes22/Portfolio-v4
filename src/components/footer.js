773601491a77e3c7c677c08e7dd41fa153f88e69
Joao Guimaraes <Joao.Guimaraes@ctr.unipartner.com>
2020-01-13 16:23:19 +0000

    Footer navigation

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,32 +15,35 @@ const Footer = () => {
       <div className="copyright">Made by João Guimaraes &copy; 2020 </div>
       <div className="icons">
         <div className="icon">
-          <a href="#landing">
+          <a href="https://github.com/JoaoGuimaraes22" target="blank">
             <FaGithub />
           </a>
         </div>
         <div className="icon">
-          <a href="#landing">
+          <a
+            href="https://www.linkedin.com/in/jo%C3%A3o-guimar%C3%A3es-4abaa7197/"
+            target="blank"
+          >
             <FaLinkedin />
           </a>
         </div>
         <div className="icon">
-          <a href="#landing">
+          <a href="https://www.instagram.com/sebasguimas/" target="blank">
             <FaInstagram />
           </a>
         </div>
         <div className="icon">
-          <a href="#landing">
+          <a href="https://codepen.io/joaoguimaraes22" target="blank">
             <FaCodepen />
           </a>
         </div>
         <div className="icon">
-          <a href="#landing">
+          <a href="https://twitter.com/sebasguimas" target="blank">
             <FaTwitter />
           </a>
         </div>
         <div className="icon">
-          <a href="#landing">
+          <a href="https://www.youtube.com/" target="blank">
             <FaYoutube />
           </a>
         </div>
